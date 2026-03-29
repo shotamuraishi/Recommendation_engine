7661c2b40d41a1bc69a4db573331d1c8fa6f3830
Shota Muraishi <shotamuraishi@mail.weber.edu>
2026-03-28 21:24:41 -0600

    Fix the bag

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import random
 import requests
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.neighbors import NearestNeighbors
@@ -11,68 +12,6 @@ st.set_page_config(layout="wide")
 
 if 'selected_movie_indices' not in st.session_state:
     st.session_state.selected_movie_indices = []
-
-def show_recommendations():
-    # When the list is empty
-    if not st.session_state.selected_movie_indices:
-        random_movies = df_movie.sample(10)
-        
-        for movie_id, movie in random_movies.iterrows():
-            with st.container():
-                col1, col2= st.columns([0.8, 0.2], vertical_alignment="center")
-                with col1:
-                    st.markdown(f"**{movie['title']}**")
-                    st.caption(f"Released year: {movie['year']}")
-                    st.caption(f"Genres: {movie['genres'].replace('|', ', ')}")
-                with col2:
-                    if st.button("Add", key=f"rand_add_{movie_id}", use_container_width=True):
-                        st.session_state.selected_movie_indices.append(movie_id)
-                        st.toast(f"Added {movie['title']}!")
-                        st.rerun()
-            st.divider()
-        return
-
-    # When movies are in the list
-    else:
-        # Calculate the 'User Profile' vector
-        user_profile = features[st.session_state.selected_movie_indices].mean(axis=0).reshape(1, -1)
-        
-        # Find nearest neighbors
-        distances, indices = model_knn.kneighbors(user_profile, n_neighbors=10)
-
-        st.write("### ✨ Recommended for You")
-        st.caption("Based on your current selections")
-
-        count = 0
-        for pos in indices.flatten():
-            movie_id = df_movie.index[pos]
-            
-            if movie_id not in st.session_state.selected_movie_indices:
-                movie = df_movie.loc[movie_id]
-                
-                with st.container():
-                    col1, col2= st.columns([0.8, 0.2], vertical_alignment="center")
-                    
-                    # Movie information
-                    with col1:
-                        st.markdown(f"**{movie['title']}**")
-                        st.caption(f"Released: {movie['year']}")
-                        st.caption(f"Genres: {movie['genres'].replace('|', ', ')}")
-                    
-                    # Add button
-                    with col2:
-                        if st.button("Add", key=f"rec_{movie_id}", use_container_width=True):
-                            st.session_state.selected_movie_indices.append(movie_id)
-                            st.toast(f"Added {movie['title']}!")
-                            st.rerun()
-                
-                st.divider()
-                count += 1
-
-            # Only display 10 movies at that same time    
-            if count > 10:
-                break
-
 
 # ----- Data -----
 df_movie = pd.read_csv('data/movies.csv', index_col='imdbId')
@@ -99,8 +38,71 @@ tfidf_features = tfidf.fit_transform(df_movie['title']).toarray()
 features = np.hstack([genre_features.values, year_features, tfidf_features])
 
 # Make a model
+random.seed(42)
 model_knn = NearestNeighbors(n_neighbors=10, metric='cosine', algorithm='brute')
 model_knn.fit(features)
+
+def show_recommendations(prefix=""):
+    # When the list is empty
+    if not st.session_state.selected_movie_indices:
+        random_movies = df_movie.sample(10, random_state=42)
+        
+        for movie_id, movie in random_movies.iterrows():
+            st.write(f"movie_id: {movie_id}, type: {type(movie_id)}")
+            with st.container():
+                col1, col2= st.columns([0.8, 0.2], vertical_alignment="center")
+                with col1:
+                    st.markdown(f"**{movie['title']}**")
+                    st.caption(f"Released year: {movie['year']}")
+                    st.caption(f"Genres: {movie['genres'].replace('|', ', ')}")
+                with col2:
+                    if st.button("Add", key=f"{prefix}rand_add_{movie_id}", use_container_width=True):
+                        st.session_state.selected_movie_indices.append(movie_id)
+                        st.toast(f"Added {movie['title']}!")
+                        st.rerun()
+            st.divider()
+        return
+
+    # When movies are in the list
+    else:
+        # Calculate the 'User Profile' vector
+        user_profile = features[st.session_state.selected_movie_indices].mean(axis=0).reshape(1, -1)
+        
+        # Find nearest neighbors
+        distances, indices = model_knn.kneighbors(user_profile, n_neighbors=10)
+
+        st.write("### ✨ Recommended for You")
+        st.caption("Based on your current selections")
+
+        count = 0
+        for pos in indices.flatten():
+            movie_id = int(pos)
+            
+            if movie_id not in st.session_state.selected_movie_indices:
+                movie = df_movie.iloc[movie_id]
+                
+                with st.container():
+                    col1, col2= st.columns([0.8, 0.2], vertical_alignment="center")
+                    
+                    # Movie information
+                    with col1:
+                        st.markdown(f"**{movie['title']}**")
+                        st.caption(f"Released: {movie['year']}")
+                        st.caption(f"Genres: {movie['genres'].replace('|', ', ')}")
+                    
+                    # Add button
+                    with col2:
+                        if st.button("Add", key=f"{prefix}rec_{movie_id}", use_container_width=True):
+                            st.session_state.selected_movie_indices.append(movie_id)
+                            st.toast(f"Added {movie['title']}!")
+                            st.rerun()
+                
+                st.divider()
+                count += 1
+
+            # Only display 10 movies at that same time    
+            if count > 10:
+                break
 
 
 
@@ -127,9 +129,9 @@ with col_main:
                         st.caption(f"Genres: {row['genres'].replace('|', ', ')}")
                     with c2:
                         if idx in st.session_state.selected_movie_indices:
-                            st.button("Selected", key=f"search_added_{idx}", disabled=True)
+                            st.button("Selected", key=f"search_added_{idx}", disabled=True, use_container_width=True)
                         else:
-                            if st.button("Add", key=f"search_add_{idx}"):
+                            if st.button("Add", key=f"search_add_{idx}", use_container_width=True):
                                 st.session_state.selected_movie_indices.append(idx)
                                 st.rerun()
                 st.divider()
@@ -137,21 +139,23 @@ with col_main:
             st.warning("No movies found.")
     else:
         st.info("Start searching to add movies to your list!")
-        show_recommendations()
+        show_recommendations(prefix="main_")
 
 # List and recommendation engine
 with col_sidebar:
+    #st.write(st.session_state.selected_movie_indices)
     st.subheader("📋 Your Selection")
     if not st.session_state.selected_movie_indices:
         st.info("Add movies to see recommendations!")
     else:
         for idx in st.session_state.selected_movie_indices:
-            m = df_movie.loc[idx]
-            col_title, col_del = st.columns([0.85, 0.15])
-            col_title.markdown(f"**{row['title']}**")
-            col_title.caption(f"Released: {row['year']}")
-            col_title.caption(f"Genres: {row['genres'].replace('|', ', ')}")
-            if col_del.button("Del", key=f"side_del_{idx}"):
+            m = df_movie.iloc[idx]
+            c1, c2 = st.columns([0.85, 0.15], vertical_alignment="center")
+            with c1:
+                st.markdown(f"**{m['title']}**")
+                st.caption(f"Released: {m['year']}")
+                st.caption(f"Genres: {m['genres'].replace('|', ', ')}")
+            if c2.button("Del", key=f"side_del_{idx}"):
                 st.session_state.selected_movie_indices.remove(idx)
                 st.rerun()
         
@@ -160,4 +164,6 @@ with col_sidebar:
             st.rerun()
 
         st.write("---")
-        show_recommendations()
+        if st.session_state.selected_movie_indices:
+            show_recommendations(prefix="side_")
+
